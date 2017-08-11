cf629035582ce525e2fca85284ec532c40c2d74e
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2017-08-11 15:11:48 +0900

    Use single quote

--- lib/trysail_blog_notification.rb
+++ lib/trysail_blog_notification.rb
@@ -1,6 +1,6 @@
 module TrySailBlogNotification; end
 
-library_files = Dir[File.join(File.dirname(__FILE__), "/**/*.rb")].sort
+library_files = Dir[File.join(File.dirname(__FILE__), '/**/*.rb')].sort
 library_files.each do |file|
   require file
 end
