ed71c24cd84fb2f3e6a57847e72ee61ebd1cc41f
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2017-08-13 21:36:24 +0900

    Use "File.join" method

--- app.rb
+++ app.rb
@@ -8,12 +8,12 @@ Bundler.require
 
 require File.join(File.dirname(__FILE__), '/lib/trysail_blog_notification.rb')
 
-config = YAML.load_file(File.expand_path('./config/config.yml'))
+config = YAML.load_file(File.join(File.dirname(__FILE__), '/config/config.yml'))
 
 twitter_client = TrySailBlogNotification::Client::TwitterClient.new(config[:twitter])
 slack_client = TrySailBlogNotification::Client::SlackClient.new(config[:slack])
 
-app = TrySailBlogNotification::Application.new(File.expand_path('data/dump.json'))
+app = TrySailBlogNotification::Application.new(File.join(File.dirname(__FILE__), 'data/dump.json'))
 app.add_client(twitter_client)
 app.add_client(slack_client)
 app.run
