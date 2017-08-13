d386edfbcde14f73aa75cf684dff421605416afa
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2017-08-13 21:38:44 +0900

    Add slash

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ config = YAML.load_file(File.join(File.dirname(__FILE__), '/config/config.yml'))
 twitter_client = TrySailBlogNotification::Client::TwitterClient.new(config[:twitter])
 slack_client = TrySailBlogNotification::Client::SlackClient.new(config[:slack])
 
-app = TrySailBlogNotification::Application.new(File.join(File.dirname(__FILE__), 'data/dump.json'))
+app = TrySailBlogNotification::Application.new(File.join(File.dirname(__FILE__), '/data/dump.json'))
 app.add_client(twitter_client)
 app.add_client(slack_client)
 app.run
