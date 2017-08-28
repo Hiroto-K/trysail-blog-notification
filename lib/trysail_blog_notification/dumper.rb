7255bdccbb55d5b1e10ea217772243bf57a3addd
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2017-08-28 22:26:17 +0900

    Add comments

--- lib/trysail_blog_notification/dumper.rb
+++ lib/trysail_blog_notification/dumper.rb
@@ -3,14 +3,31 @@ require 'json'
 module TrySailBlogNotification
   class Dumper
 
+    # Dump file path.
+    #
+    # @return String
     attr_reader :file
+
+    # Statuses.
+    #
+    # @return Hash
     attr_reader :statuses
+
+    # JSON statuses.
+    #
+    # @return String
     attr_reader :json_statuses
 
+    # Initialize dumper.
+    #
+    # @param [String] file Dump file path.
     def initialize(file)
       @file = file
     end
 
+    # Dump to file.
+    #
+    # @param [Hash] statuses Current statues.
     def dump(statuses)
       @statuses = statuses
       @json_statuses = JSON.pretty_generate(@statuses)
