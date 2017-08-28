481faa6d0d9418c030611b592c0c49d222fb1c9c
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2017-08-28 22:31:42 +0900

    Update base client class

--- lib/trysail_blog_notification/client/base_client.rb
+++ lib/trysail_blog_notification/client/base_client.rb
@@ -1,4 +1,24 @@
 module TrySailBlogNotification::Client
   class BaseClient
+
+    # Application instance.
+    #
+    # @return [TrySailBlogNotification::Application]
+    attr_reader :app
+
+    # Config hash.
+    #
+    # @return [Hash]
+    attr_reader :config
+
+    # Initialize client.
+    #
+    # @param [TrySailBlogNotification::Application] app Application instance.
+    # @param [Hash] config Config hash.
+    def initialize(app, config)
+      @app = app
+      @config = config
+    end
+
   end
 end
