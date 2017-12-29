c67ca1e76b9c925ddc98576c2e105ece9d3fb011
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2017-12-29 23:45:57 +0900

    Split the some methods of TrySailBlogNotification::Parser::User::ShiinaNatsukawa

--- lib/trysail_blog_notification/parser/user/shiina_natsukawa.rb
+++ lib/trysail_blog_notification/parser/user/shiina_natsukawa.rb
@@ -6,15 +6,48 @@ module TrySailBlogNotification::Parser::User
     # @param [Nokogiri::HTML::Document] nokogiri
     # @return [TrySailBlogNotification::LastArticle]
     def parse(nokogiri)
-      articles = nokogiri.xpath('//div[@class="skinMainArea2"]/article[@class="js-entryWrapper"]')
-      first_article = articles.first
+      @nokogiri = nokogiri
 
-      title_obj = first_article.xpath('//h1/a[@class="skinArticleTitle"]').first
-      title = title_obj.children.first.content.strip
+      first_article = get_top_page_articles.first
+      title_obj = get_title_obj(first_article)
+      title = get_title(title_obj)
       url = title_obj.attributes['href'].value
-      last_update = first_article.xpath('//span[@class="articleTime"]//time').first.content
+      last_update = get_last_update(first_article)
 
       TrySailBlogNotification::LastArticle.new(title, url, last_update)
+    end
+
+    private
+
+    # Get top page articles.
+    #
+    # @return [Nokogiri::XML::NodeSet]
+    def get_top_page_articles
+      @nokogiri.xpath('//div[@class="skinMainArea2"]/article[@class="js-entryWrapper"]')
+    end
+
+    # Get title object.
+    #
+    # @param [Nokogiri::XML::Element] article
+    # @return [Nokogiri::XML::Element]
+    def get_title_obj(article)
+      article.xpath('//h1/a[@class="skinArticleTitle"]').first
+    end
+
+    # Get title.
+    #
+    # @param [Nokogiri::XML::Element]
+    # @return [String]
+    def get_title(title_obj)
+      title_obj.children.first.content.strip
+    end
+
+    # Get last update date.
+    #
+    # @param [Nokogiri::XML::Element]
+    # @return [String]
+    def get_last_update(article)
+      article.xpath('//span[@class="articleTime"]//time').first.content
     end
 
   end
