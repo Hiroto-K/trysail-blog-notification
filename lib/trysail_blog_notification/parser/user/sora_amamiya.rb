743b10994e2b984917e2b830131a15f15be76895
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2017-12-29 23:12:43 +0900

    Implement TrySailBlogNotification::Parser::User::SoraAmamiya

---
+++ lib/trysail_blog_notification/parser/user/sora_amamiya.rb
@@ -0,0 +1,21 @@
+module TrySailBlogNotification::Parser::User
+  class SoraAmamiya < TrySailBlogNotification::Parser::BaseParser
+
+    # Start parse.
+    #
+    # @param [Nokogiri::HTML::Document] nokogiri
+    # @return [TrySailBlogNotification::LastArticle]
+    def parse(nokogiri)
+      articles = nokogiri.xpath('//div[@class="skinMainArea2"]/article[@class="js-entryWrapper"]')
+      first_article = articles.first
+
+      title_obj = first_article.xpath('//h1/a[@class="skinArticleTitle"]').first
+      title = title_obj.children.first.content.strip
+      url = title_obj.attributes['href'].value
+      last_update = first_article.xpath('//span[@class="articleTime"]//time').first.content
+
+      TrySailBlogNotification::LastArticle.new(title, url, last_update)
+    end
+
+  end
+end
