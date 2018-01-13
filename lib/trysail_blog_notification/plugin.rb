a8e40fae83e8d669f58d2ea32554f5eccefcc960
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-01-14 01:01:03 +0900

    Implement initialize method

--- lib/trysail_blog_notification/plugin.rb
+++ lib/trysail_blog_notification/plugin.rb
@@ -1,4 +1,17 @@
 module TrySailBlogNotification
   class Plugin
+
+    # Base dir path
+    #
+    # @return [String]
+    attr_reader :base_dir
+
+    # Initialize Plugin
+    #
+    # @param [String] base_dir Base dir path
+    def initialize(base_dir)
+      @base_dir = base_dir
+    end
+
   end
 end
