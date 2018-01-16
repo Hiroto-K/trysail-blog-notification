d34fa238a54d563b81a07f28238cde7e478c6b8a
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-01-16 23:57:01 +0900

    Fix the file name

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ ENV['BUNDLE_GEMFILE'] = File.join(File.dirname(__FILE__), 'Gemfile')
 require 'bundler'
 Bundler.require
 
-require File.join(File.dirname(__FILE__), '/lib/trysail_blog_notification.rb')
+require File.join(File.dirname(__FILE__), '/lib/try_sail_blog_notification.rb')
 
 config = YAML.load_file(File.join(File.dirname(__FILE__), '/config/config.yml'))
 
