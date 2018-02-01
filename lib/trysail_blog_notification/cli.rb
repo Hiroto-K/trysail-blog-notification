f07190bfa1e5886542628eaec1759b14fe667c6d
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-02-01 17:55:27 +0900

    Using single quote

--- lib/trysail_blog_notification/cli.rb
+++ lib/trysail_blog_notification/cli.rb
@@ -1,7 +1,7 @@
 module TrySailBlogNotification
   class Cli < Thor
 
-    desc "start", "Run trysail-blog-notification."
+    desc 'start', 'Run trysail-blog-notification.'
     def start
       command = TrySailBlogNotification::Command::StartCommand.new
       command.start
