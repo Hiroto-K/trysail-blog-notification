17c44ff65b46fab6920f6fbe1a8d50a1c2d8fff3
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-02-01 18:05:30 +0900

    Set package name

--- lib/trysail_blog_notification/cli.rb
+++ lib/trysail_blog_notification/cli.rb
@@ -1,6 +1,8 @@
 module TrySailBlogNotification
   class Cli < Thor
 
+    package_name 'trysail-blog-notification'
+
     desc 'start', 'Run trysail-blog-notification.'
     def start
       command = TrySailBlogNotification::Command::StartCommand.new
