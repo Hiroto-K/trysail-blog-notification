a38157bad9f0314c8c72432bb741bafbe779afb2
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-02-01 18:15:39 +0900

    Set options and args.

--- lib/trysail_blog_notification/command/base_command.rb
+++ lib/trysail_blog_notification/command/base_command.rb
@@ -1,6 +1,25 @@
 module TrySailBlogNotification::Command
   class BaseCommand
 
+    # Command options.
+    #
+    # @return [Hash]
+    attr_reader :options
+
+    # Command args.
+    #
+    # @return [Array]
+    attr_reader :args
+
+    # Initialize Command.
+    #
+    # @param [Hash] options
+    # @param [Array] args
+    def initialize(options, args)
+      @options = options
+      @args = args
+    end
+
     # Start command.
     def start
     end
