cd07052cd1ab0f1f3beee54902834ac42a1560bb
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-02-02 19:04:18 +0900

    Set the description

--- lib/trysail_blog_notification/cli.rb
+++ lib/trysail_blog_notification/cli.rb
@@ -6,7 +6,7 @@ module TrySailBlogNotification
     package_name 'trysail-blog-notification'
 
     desc 'start', 'Run trysail-blog-notification.'
-    option 'no-notification', type: :boolean, default: false
+    option 'no-notification', desc: 'No send the notification.', type: :boolean, default: false
     def start
       command = TrySailBlogNotification::Command::StartCommand.new(options, args)
       command.start
