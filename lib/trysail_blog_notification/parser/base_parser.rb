5631437f43d873f4014d81f03fe07bb839cd0537
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-02-14 18:04:21 +0900

    Implement BaseParser.build_url method

--- lib/trysail_blog_notification/parser/base_parser.rb
+++ lib/trysail_blog_notification/parser/base_parser.rb
@@ -18,6 +18,7 @@ module TrySailBlogNotification::Parser
     def initialize(app, config)
       @app = app
       @config = config
+      @blog_host = 'https://ameblo.jp/'
     end
 
     # Start parse.
@@ -25,6 +26,16 @@ module TrySailBlogNotification::Parser
     # @param [Nokogiri::HTML::Document] nokogiri
     # @return [TrySailBlogNotification::LastArticle]
     def parse(nokogiri)
+    end
+
+    private
+
+    # Build url from path
+    #
+    # @param [String] path
+    # @return [String]
+    def build_url(path)
+      URI.join(@blog_host, path).to_s
     end
 
   end
