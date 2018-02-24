28e3883bfa93ba5be1a84fb415005723bd255c2c
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-02-25 04:12:38 +0900

    Load test file

--- test.rb
+++ test.rb
@@ -4,4 +4,6 @@ require 'bundler'
 Bundler.require(:default, :test)
 
 require File.join(File.dirname(__FILE__), '/lib/trysail_blog_notification.rb')
+require File.join(File.dirname(__FILE__), '/test/trysail_blog_notification.rb')
+
 Dir['./test/**/*.rb'].sort.each(&method(:require))
