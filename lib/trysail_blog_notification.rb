dc42fb53642174b05e7c08ab909a635e2effd846
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-03-13 19:03:44 +0900

    Load application and util in base file

--- lib/trysail_blog_notification.rb
+++ lib/trysail_blog_notification.rb
@@ -1,6 +1,10 @@
 module TrySailBlogNotification
 end
 
+require File.join(File.dirname(__FILE__), '/trysail_blog_notification/application.rb')
+require File.join(File.dirname(__FILE__), '/trysail_blog_notification/util.rb')
+
 Dir[File.join(File.dirname(__FILE__), '/**/*.rb')].sort.each do |file|
+  pp file
   require file
 end
