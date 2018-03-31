631e21a8bf5301637ea1b660f4374ad141dda66e
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-03-31 14:07:06 +0900

    Remove asterisk

--- test/trysail_blog_notification/test_last_article.rb
+++ test/trysail_blog_notification/test_last_article.rb
@@ -15,13 +15,13 @@ module TrySailBlogNotificationTest
     end
 
     def test_to_h
-      last_article = TrySailBlogNotification::LastArticle.new(**@default_argument)
+      last_article = TrySailBlogNotification::LastArticle.new(@default_argument)
 
       assert_instance_of(Hash, last_article.to_h)
     end
 
     def test_hash_access_string
-      last_article = TrySailBlogNotification::LastArticle.new(**@default_argument)
+      last_article = TrySailBlogNotification::LastArticle.new(@default_argument)
 
       assert_equal(@title, last_article['title'])
       assert_equal(@url, last_article['url'])
@@ -29,7 +29,7 @@ module TrySailBlogNotificationTest
     end
 
     def test_hash_access_symbol
-      last_article = TrySailBlogNotification::LastArticle.new(**@default_argument)
+      last_article = TrySailBlogNotification::LastArticle.new(@default_argument)
 
       assert_equal(@title, last_article[:title])
       assert_equal(@url, last_article[:url])
@@ -37,13 +37,13 @@ module TrySailBlogNotificationTest
     end
 
     def test_hash_access_error_string
-      last_article = TrySailBlogNotification::LastArticle.new(**@default_argument)
+      last_article = TrySailBlogNotification::LastArticle.new(@default_argument)
 
       assert_nil(last_article['example'])
     end
 
     def test_hash_access_error_symbol
-      last_article = TrySailBlogNotification::LastArticle.new(**@default_argument)
+      last_article = TrySailBlogNotification::LastArticle.new(@default_argument)
 
       assert_nil(last_article[:example])
     end
