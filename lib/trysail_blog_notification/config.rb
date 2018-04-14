0ea9435e877df3c7f44661d246c89f6a69bb9296
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-04-14 21:15:18 +0900

    Create initialize method

--- lib/trysail_blog_notification/config.rb
+++ lib/trysail_blog_notification/config.rb
@@ -2,5 +2,15 @@
 
 module TrySailBlogNotification
   class Config
+
+    attr_reader :raw_config
+
+    # Initialize TrySailBlogNotification::Config
+    #
+    # @param [Hash] config
+    def initialize(config = {})
+      @raw_config = config
+    end
+
   end
 end
