9f7854c1ce85c89ede9c8bab9a1bdbc7910e8fb4
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-05-11 22:33:20 +0900

    Create find_client

--- lib/trysail_blog_notification/command/client_test_command.rb
+++ lib/trysail_blog_notification/command/client_test_command.rb
@@ -5,6 +5,20 @@ module TrySailBlogNotification::Command
 
     # Start command.
     def start
+      name = args[:name]
+      client = find_client(name)
+    end
+
+    private
+
+    # Find client
+    #
+    # @param name [String]
+    # @return [Array]
+    def find_client(name)
+      clients = app.config.get(:client)
+      raise "Client '#{name}' not found." if clients[name].nil?
+      clients[name]
     end
 
   end
