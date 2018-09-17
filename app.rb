8934977a422434ecd8236a8aed20d0a0de5b1750
Hiroto Kitazawa <hiro.yo.yo1610@gmail.com>
2018-09-17 20:53:47 +0900

    Remove and fix the Style/Dir cop

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ require File.join(File.dirname(__FILE__), '/lib/trysail_blog_notification.rb')
 config = YAML.load_file(File.join(File.dirname(__FILE__), '/config/config.yml'))
 
 app = TrySailBlogNotification::Application.new(
-  File.expand_path(File.dirname(__FILE__)),
+  __dir__,
   config
 )
 
